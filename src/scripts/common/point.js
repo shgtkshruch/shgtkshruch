73fcc5f1c56332633251ddf906e12ad936915af9
shgtkshruch <shgtk.shruch@gmail.com>
2016-12-22 19:30:59 +0900

    Add close effect

--- src/scripts/common/point.js
+++ src/scripts/common/point.js
@@ -1,11 +1,13 @@
 (() => {
   const $icon = $('.js-point-icon');
+  const $close = $('.js-close');
   const padding = 40;
 
   $icon.click(function () {
     const targets = this;
     const $targets = $(targets);
-    const $content = $(targets).next();
+    const $parent = $targets.parent();
+    const $content = $parent.find('.screenshot__desc');
 
     $content.show();
     const width = $content.width() + padding * 2;
@@ -32,5 +34,41 @@
       }
     });
 
+  });
+
+  $icon.parent().hover(function (e) {
+    if ($(this).find('.screenshot__icon').hasClass('is-active')) {
+      $(this).find('.screenshot__close').fadeIn();
+    }
+  }, function (e) {
+    if ($(this).find('.screenshot__icon').hasClass('is-active')) {
+      $(this).find('.screenshot__close').fadeOut();
+    }
+  });
+
+  $close.click(function (e) {
+    const $targets = $(this);
+
+    $targets.fadeOut();
+    $targets.siblings('.screenshot__desc').fadeOut(function () {
+      anime({
+        targets: $targets.siblings('.screenshot__icon').get(0),
+        height: '40px',
+        duration: 800,
+        easing: 'easeOutExpo',
+        complete() {
+          anime({
+            targets: this.targets,
+            width: '40px',
+            borderRadius: '50%',
+            duration: 800,
+            easing: 'easeOutElastic',
+            complete() {
+              $(this.targets).removeClass('is-active');
+            }
+          });
+        }
+      });
+    });
   });
 })();
